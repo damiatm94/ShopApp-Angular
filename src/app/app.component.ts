26bc1fd8575ef967cd4082f51b169c8df6d5cfb0
damiatm94 <damkamg94@gmail.com>
2019-09-22 14:27:55 +0200

    Added simple tab switching

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shop-app-angular';
+  private tabNames = ['Warehouse', 'Orders', 'Shop'];
+  private activeTabName = this.tabNames[0];
+
+  private changeActiveTab(tabName: string) {
+    this.activeTabName = tabName;
+  }
 }
